8f7e72fa7e3bb5e134949a7deac2c83f8bc5f801
Shubham Kumar <shubham.sondhiya@gmail.com>
2021-09-11 16:46:36 +0530

    Fourth Commit

--- src/main/java/com/icinbank/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.icinbank.service;
-
-import com.icinbank.details.LoginDetails;
-import com.icinbank.response.LoginResponse;
-
-public interface LoginService {
-
-	public LoginResponse customerLogin(LoginDetails details);
-}
